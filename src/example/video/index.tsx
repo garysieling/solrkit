a3c346c63dab6a72f88c81b8b48ede2a393082ed
gary <gary@garysieling.com>
2017-10-15 19:08:54 -0400

    Move all demo app

---
+++ src/example/video/index.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import registerServiceWorker from '../../registerServiceWorker';
+import './index.css';
+
+import { NotFound } from '../../pages/NotFound';
+import { Search } from '../../pages/Search';
+
+import { Route } from 'react-router';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import { DetailPageApp } from './DetailPage';
+
+interface RouterProps {
+  history: object;
+  location: object;
+  match: {
+    isExact: boolean;
+    params: {
+      id: string;
+    };
+    path: string;
+    url: string;
+  };
+}
+
+import createHistory from 'history/createBrowserHistory';
+const history = createHistory({
+  basename: '/view/',
+  hashType: 'noslash'
+});
+
+class DetailComponent extends React.Component<RouterProps, {id: string}> {
+  constructor(props: RouterProps) {
+    super();
+
+    this.state = {
+      id: props.match.params.id
+    };
+
+    history.listen((location, action) => {
+      if (location.key) {
+        this.setState({
+          id: location.pathname.substring(1)
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <DetailPageApp 
+        load={(id: string) => {
+          history.push(id, {});
+        }}
+        id={this.state.id}
+      />
+    );
+  }
+}
+
+class Routes extends React.Component<{}, {}> {
+  constructor() {
+    super();
+  }
+
+  render() {    
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={Search} />
+          <Route 
+            path="/view/:id" 
+            component={DetailComponent} 
+          />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export function main() {
+  ReactDOM.render(
+    <Routes />,
+    document.getElementById('root') as HTMLElement
+  );
+
+  registerServiceWorker();
+}
