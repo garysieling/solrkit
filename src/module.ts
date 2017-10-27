9cedc51b29f5a537dc4f772e2fde5d22d2a0b449
gary <gary@garysieling.com>
2017-10-26 23:09:12 -0400

    module

--- src/module.ts
+++ src/module.ts
@@ -1,17 +1,41 @@
 import * as DataBinding from './context/DataBinding';
 import * as DataStore from './context/DataStore';
+import * as Data from './context/Data';
 
 const context = {
   DataBinding,
-  DataStore
+  DataStore,
+  Data
 };
 
+import * as ExcelExport from './component/ExcelExport';
 import * as MoreLikeThis from './component/MoreLikeThis';
-import * as SearchBox from './component/MoreLikeThis';
+import * as Pagination from './component/Pagination';
+import * as ResultsList from './component/ResultsList';
+import * as SearchBox from './component/SearchBox';
 
 const components = {
+  ExcelExport,
   MoreLikeThis,
+  Pagination,
+  ResultsList,
   SearchBox
+};
+
+import * as CheckFacet from './component/facet/CheckFacet';
+import * as DropdownFacet from './component/facet/DropdownFacet';
+import * as FacetTypes from './component/facet/FacetTypes';
+import * as RadioFacet from './component/facet/RadioFacet';
+import * as TagFacet from './component/facet/TagFacet';
+import * as ToggleFacet from './component/facet/ToggleFacet';
+
+const facets = {
+  CheckFacet,
+  DropdownFacet,
+  FacetTypes,
+  RadioFacet,
+  TagFacet,
+  ToggleFacet
 };
 
 import * as DetailLayout from './layout/DetailLayout';
@@ -22,4 +46,4 @@ const layout = {
   ResultsLayout
 };
 
-export { context, components, layout };
+export { context, components, facets, layout };
