cbc53d50fb932613f8f5422a723a0bef4435fe24
gary <gary@garysieling.com>
2017-10-28 09:33:36 -0400

    Fixes to get this working as a module

--- src/module.ts
+++ src/module.ts
@@ -1,18 +1,20 @@
 import * as DataBinding from './context/DataBinding';
-import * as DataStore from './context/DataStore';
-import * as Data from './context/Data';
+import { 
+  DataStore 
+} from './context/DataStore';
+import { databind } from './context/DataBinding';
 
 const context = {
   DataBinding,
   DataStore,
-  Data
+  databind
 };
 
-import * as ExcelExport from './component/ExcelExport';
-import * as MoreLikeThis from './component/MoreLikeThis';
-import * as Pagination from './component/Pagination';
-import * as ResultsList from './component/ResultsList';
-import * as SearchBox from './component/SearchBox';
+import { ExcelExport } from './component/ExcelExport';
+import { MoreLikeThis } from './component/MoreLikeThis';
+import { Pagination } from './component/Pagination';
+import { ResultsList } from './component/ResultsList';
+import { SearchBox } from './component/SearchBox';
 
 const components = {
   ExcelExport,
@@ -22,24 +24,22 @@ const components = {
   SearchBox
 };
 
-import * as CheckFacet from './component/facet/CheckFacet';
-import * as DropdownFacet from './component/facet/DropdownFacet';
-import * as FacetTypes from './component/facet/FacetTypes';
-import * as RadioFacet from './component/facet/RadioFacet';
-import * as TagFacet from './component/facet/TagFacet';
-import * as ToggleFacet from './component/facet/ToggleFacet';
+import { CheckFacet } from './component/facet/CheckFacet';
+import { DropdownFacet } from './component/facet/DropdownFacet';
+import { RadioFacet } from './component/facet/RadioFacet';
+import { TagFacet } from './component/facet/TagFacet';
+import { ToggleFacet } from './component/facet/ToggleFacet';
 
 const facets = {
   CheckFacet,
   DropdownFacet,
-  FacetTypes,
   RadioFacet,
   TagFacet,
   ToggleFacet
 };
 
-import * as DetailLayout from './layout/DetailLayout';
-import * as ResultsLayout from './layout/ResultsLayout';
+import { DetailLayout } from './layout/DetailLayout';
+import { ResultsLayout } from './layout/ResultsLayout';
 
 const layout = {
   DetailLayout,
